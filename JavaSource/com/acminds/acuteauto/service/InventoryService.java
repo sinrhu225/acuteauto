4fe4756a179dc2ef72336029368f16ce433b3138
Mansur Md <Mansur.Md@gmail.com>
2012-06-18 21:07:13 -0400

    Initial Push of Inventory Maintenance changes

--- JavaSource/com/acminds/acuteauto/service/InventoryService.java
+++ JavaSource/com/acminds/acuteauto/service/InventoryService.java
@@ -6,11 +6,15 @@ package com.acminds.acuteauto.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
+
 import com.acminds.acuteauto.persistence.dao.InventoryDAO;
 import com.acminds.acuteauto.persistence.dto.Advertisement;
+import com.acminds.acuteauto.persistence.dto.Feature;
 import com.acminds.acuteauto.persistence.dto.Make;
 import com.acminds.acuteauto.persistence.dto.Vehicle;
 import com.acminds.acuteauto.utils.Utils;
+import com.acminds.acuteauto.utils.WebUtils;
 
 
 /**
@@ -53,8 +57,31 @@ public class InventoryService extends BaseService {
 		return dao.getCarsByCategory(id, name);
 	}
 	
-	public void saveOrUpdateCar() {
-		
+	public void saveOrUpdateCar(Vehicle car) throws Exception{
+
+		try {
+			List<Feature> rem = new ArrayList<Feature>();
+			for(Feature p:car.getFeatures()) {
+				if(!car.getSelectedFeatures().contains(p)) {
+					p.getVehicles().remove(car);
+					rem.add(p);						
+				}					
+			}
+			car.getFeatures().removeAll(rem);
+			for(Feature p:car.getSelectedFeatures()) {
+				if(!car.getFeatures().contains(p)) {
+					car.getFeatures().add(p);
+					p.getVehicles().add(car);
+				}
+				saveOrUpdate(p, false);
+			}
+			saveOrUpdate(car, false);
+			commit();
+			logger.info("Vehicle saved successfully.");			
+		} catch(Exception e) {
+			rollback();
+			logger.error("Role could not be saved due to an internal error.", e);			
+		}
 	}
 	
 	public void saveOrUpdateCarImages() {
