eb2064aa7cffc1b7938d56bafa57cccf5397f693
Mansur Md <Mansur.Md@gmail.com>
2012-07-01 12:29:46 -0400

    Quick Add Make/Model/Style

--- JavaSource/com/acminds/acuteauto/ui/controller/CatalogController.java
+++ JavaSource/com/acminds/acuteauto/ui/controller/CatalogController.java
@@ -6,12 +6,11 @@ package com.acminds.acuteauto.ui.controller;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.event.ComponentSystemEvent;
+import javax.faces.event.AjaxBehaviorEvent;
 
 import com.acminds.acuteauto.persistence.dto.Make;
 import com.acminds.acuteauto.persistence.dto.Model;
 import com.acminds.acuteauto.persistence.dto.Style;
-import com.acminds.acuteauto.ui.BaseController;
 import com.acminds.acuteauto.utils.WebUtils;
 
 /**
@@ -20,90 +19,63 @@ import com.acminds.acuteauto.utils.WebUtils;
  */
 @ManagedBean(name="ctlCtrl")
 @ViewScoped
-public class CatalogController extends BaseController {
+public class CatalogController extends InventoryController {
 
 	private Make make;
 	private Model model;
 	private Style style;
-	/**
-	 * @return the make
-	 */
+	
 	public Make getMake() {
 		return make;
 	}
-	/**
-	 * @param make the make to set
-	 */
 	public void setMake(Make make) {
 		this.make = make;
 	}
-	/**
-	 * @return the model
-	 */
 	public Model getModel() {
 		return model;
 	}
-	/**
-	 * @param model the model to set
-	 */
 	public void setModel(Model model) {
 		this.model = model;
 	}
-	/**
-	 * @return the style
-	 */
 	public Style getStyle() {
 		return style;
 	}
-	/**
-	 * @param style the style to set
-	 */
 	public void setStyle(Style style) {
 		this.style = style;
 	}
 	
-	public void loadMake(ComponentSystemEvent e) {
-		if(make == null) {
-			make = new Make();
-		}
+	public String prepareCatalog() {
+		this.make = new Make();
+		this.model = new Model();
+		this.style = new Style();
+		return null;
 	}
 	
-	public void loadModel(ComponentSystemEvent e) {
-		if(model == null) {
-			model = new Model();
-			if(make!=null)
-				model.setMake(make);
-		}
-	}
-	
-	public void loadStyle(ComponentSystemEvent e) {
-		if(style == null) {
-			style = new Style();
-			if(make!=null)
-				model.setMake(make);
-			if(model!=null)
-				style.setModel(model);			
-		}
-	}
-	
-	public String submitMake() {
+	public void submitMake(AjaxBehaviorEvent abe) {
 		try {
 			logger.info("Saving Make.");
 			baseService.saveOrUpdate(make, true);
+			baseService.refresh(make);
+			getAllMakes().add(make);
+			// TO PRESELECT MAKE WHILE CREATING NEW MODEL 
+			model.setMake(make);
 			logger.info("Make saved successfully.");
 			WebUtils.addMessage(FacesMessage.SEVERITY_INFO, "saveMakeSuccessful");
 		} catch(Exception e) {
 			baseService.rollback();
 			logger.error("Make could not be saved due to an internal error.", e);
 			WebUtils.addMessage(FacesMessage.SEVERITY_ERROR, "submitFailed");
-		}
-		return null;
+		}		
 	}
 	
-	public String submitModel() {
+	public void submitModel(AjaxBehaviorEvent abe) {
 		try {
 			logger.info("Saving Model.");
 			baseService.saveOrUpdate(model, true);
+			baseService.refresh(model);
+			// TO PRESELECT MODEL WHILE CREATING NEW STYLE
+			style.setModel(model);
+			style.setMake(model.getMake());
 			logger.info("Model saved successfully.");
 			WebUtils.addMessage(FacesMessage.SEVERITY_INFO, "saveModelSuccessful");
 		} catch(Exception e) {
@@ -111,13 +83,13 @@ public class CatalogController extends BaseController {
 			logger.error("Model could not be saved due to an internal error.", e);
 			WebUtils.addMessage(FacesMessage.SEVERITY_ERROR, "submitFailed");
 		}
-		return null;
 	}
 	
-	public String submitStyle() {
+	public void submitStyle(AjaxBehaviorEvent abe) {
 		try {
 			logger.info("Saving Style.");
 			baseService.saveOrUpdate(style, true);
+			baseService.refresh(style);
 			logger.info("Style saved successfully.");
 			WebUtils.addMessage(FacesMessage.SEVERITY_INFO, "saveStyleSuccessful");
 		} catch(Exception e) {
@@ -125,7 +97,6 @@ public class CatalogController extends BaseController {
 			logger.error("Style could not be saved due to an internal error.", e);
 			WebUtils.addMessage(FacesMessage.SEVERITY_ERROR, "submitFailed");
 		}
-		return null;
 	}
 	
 	
