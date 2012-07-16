34518b766b1f481d16ad0d043c19543bb401d9f5
Mansur Md <Mansur.Md@gmail.com>
2012-07-16 00:38:35 -0400

    ConfigManager updated

--- JavaSource/com/acminds/acuteauto/ConfigManager.java
+++ JavaSource/com/acminds/acuteauto/ConfigManager.java
@@ -3,10 +3,26 @@
  */
 package com.acminds.acuteauto;
 
+import java.util.Properties;
+
+import com.acminds.acuteauto.utils.Utils;
+
 /**
  * @author Mansur
  *
  */
 public class ConfigManager {
-
+	private static final String APP_CONFIG = "cfg/App-Config.properties";
+	private static Properties props = new Properties();
+	static {
+		try {
+			props.load(Utils.getResource(APP_CONFIG));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static String getValue(String key) {
+		return props.getProperty(key);
+	}
 }
